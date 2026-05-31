ea8a0883fb9a79d3b34839a8533fb44d0a733e46
anchal.koshta007@gmail.com <genious@genious.genious>
2026-05-31 20:51:18 +0530

    password Issue2

--- utils/db-connections.js
+++ utils/db-connections.js
@@ -1,15 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-// Parameters ko alag rakhne se password ka special character error nahi dega
 const sequelize = new Sequelize('postgres', 'postgres', 'A1jadu_hai%@', {
-    host: 'aws-1-ap-northeast-1.pooler.supabase.com',
-    port: 6543,
+    host: '://supabase.com', // Host yahi rahega
+    port: 5432,                                       // Port ko 6543 se badal kar 5432 kar diya
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
         ssl: {
             require: true,
-            rejectUnauthorized: false // Supabase remote cloud server ke liye zaroori hai
+            rejectUnauthorized: false
         }
     }
 });
