e20ec28ddec90adb18be0fc581b43df9b059a0b9
anchal.koshta007@gmail.com <genious@genious.genious>
2026-05-31 20:58:54 +0530

    password Issue3

--- utils/db-connections.js
+++ utils/db-connections.js
@@ -1,14 +1,15 @@
 const { Sequelize } = require('sequelize');
 
+// Seedhe Supabase ka details bina kisi .env file ke
 const sequelize = new Sequelize('postgres', 'postgres', 'A1jadu_hai%@', {
-    host: '://supabase.com', // Host yahi rahega
-    port: 5432,                                       // Port ko 6543 se badal kar 5432 kar diya
+    host: '://supabase.com',
+    port: 5432, // Port 5432 sabse best hai cloud database ke liye
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
         ssl: {
             require: true,
-            rejectUnauthorized: false
+            rejectUnauthorized: false // Cloud database connections ke liye yeh line compulsory hai
         }
     }
 });
@@ -18,7 +19,7 @@ const sequelize = new Sequelize('postgres', 'postgres', 'A1jadu_hai%@', {
         await sequelize.authenticate();
         console.log("Database has been connected to Supabase successfully....!");
     } catch (error) {
-        console.error("DB not connected... Error details:", error);
+        console.error("DB not connected... Error details:", error.message);
     }
 })();
 
