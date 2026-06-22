403e5937aacb1b1f960b2d8d0f9c5b6d901150cb
anchal.koshta007@gmail.com <genious@genious.genious>
2026-06-22 11:05:32 +0530

    signIn with correction8

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const  { addUser}  = require('../controllers/userController');
 const auth = require('../middleWare/auth');
 
-router.get('/addUser', auth, addUser);
+router.post('/addUser', auth, addUser);
 
 
 
